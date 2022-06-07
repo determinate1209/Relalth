f1d69249d9286942e2a297892d80f5d46afaede2
determinate1209 <determinate1209@gmail.com>
2022-06-07 10:05:43 +0000

    ルーティング調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,4 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'menu_items/edit'
-  end
-  namespace :public do
-    get 'menus/new'
-    get 'menus/index'
-    get 'menus/show'
-    get 'menus/edit'
-  end
-  namespace :public do
-    get 'hearings/new'
-    get 'hearings/show'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/quit'
-  end
-  namespace :admin do
-    get 'comments/index'
-  end
-  namespace :admin do
-    get 'menus/new'
-    get 'menus/show'
-  end
-  namespace :admin do
-    get 'plans/index'
-    get 'plans/show'
-    get 'plans/edit'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
   #顧客
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
@@ -44,6 +9,29 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: 'admin/sessions'
   }
-
+  
+  root to: 'public/homes#top'#top
+  
+ 
+  scope module: :public do
+    get 'about'
+    get 'bookmark' => 'bookmarks#index'#お気に入り一覧
+    resources :menus, only: [:new, :index, :show, :edit, :create, :update] do #メニュー機能
+      resource :menu_items, only: [:edit, :create, :update]#メニューアイテム関連機能
+      resource :comments, only: [:create, :destroy]#コメント機能
+      resource :bookmarks, only: [:create, :destroy]#お気に入り機能
+    end
+    resources :hearings, only: [:new, :show, :create]#顧客診断機能
+    resources :customers, only: [:show, :edit, :update]#顧客会員情報機能
+    get 'customers/quit' => 'customers#quit'#退会確認ページ
+    patch 'customers/withdraw' => 'customers#withdraw'#退会処理
+  end
+  
+  namespace :admin do
+    resources :comments, only: [:index, :destroy] #管理者コメント一覧機能
+    resources :menus, only: [:new, :show, :create, :edit, :update, :destroy]#管理者メニュー機能
+    resources :plans, only: [:index, :show, :edit, :create, :update, :destroy]#管理者プラン作成機能
+    resources :customers, only: [:index, :show, :edit, :update]#管理者会員情報機能
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
