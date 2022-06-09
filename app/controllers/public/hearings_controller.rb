e4a6dd904a018a0091baac652222d129d8917ae5
determinate1209 <determinate1209@gmail.com>
2022-06-09 04:32:38 +0000

    診断機能実装完了

--- app/controllers/public/hearings_controller.rb
+++ app/controllers/public/hearings_controller.rb
@@ -1,7 +1,25 @@
 class Public::HearingsController < ApplicationController
   def new
+    @hearing = Hearing.new
   end
 
   def show
+    @hearing = Hearing.find(params[:id])
+  end
+  
+  def create
+    @hearing = Hearing.new(hearing_params)
+    params[:hearing][:question] ? @hearing.question = params[:hearing][:question].join("") : false
+    if @hearing.save
+      redirect_to hearing_path(@hearing.id)
+    else
+      root_path
+    end
+  end
+  
+  private
+  
+  def hearing_params
+    params.require(:hearing).permit(:id, question: [])
   end
 end
