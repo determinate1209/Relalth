76e853fed50df18dc016a8201afc8038e7b3260e
determinate1209 <determinate1209@gmail.com>
2022-06-10 12:27:02 +0000

    メニュー機能　実装開始

--- app/controllers/public/menus_controller.rb
+++ app/controllers/public/menus_controller.rb
@@ -1,5 +1,14 @@
 class Public::MenusController < ApplicationController
   def new
+    @menu = Menu.new
+  end
+  
+  def create
+    @menu = Menu.new(menu_params)
+    @menu.customer = current_customer.id #誰が投稿したかわかるように
+    
+    @menu.create
+    redirect_to 
   end
 
   def index
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,18 @@
 class Menu < ApplicationRecord
   belongs_to :plan
+  belongs_to:customer	
+  has_many :menu_items	
+  has_many :comments	
+  has_many :bookmarks	
+  
+  has_one_attached :image
+  
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image.variant(resize_to_limit: [width, height]).processed
+  end
+  
 end
