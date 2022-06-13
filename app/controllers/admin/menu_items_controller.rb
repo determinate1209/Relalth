94d98a4a8743369d65c39114bf44967655d83bbe
determinate1209 <determinate1209@gmail.com>
2022-06-13 11:27:32 +0000

    メニュアイテム管理者非同期化

--- app/controllers/admin/menu_items_controller.rb
+++ app/controllers/admin/menu_items_controller.rb
@@ -1,6 +1,7 @@
 class Admin::MenuItemsController < ApplicationController
   def destroy
-    menu = Menu.find(params[:menu_id])
+    refroute = Rails.application.routes.recognize_path(request.referrer)
+    @menu = Menu.find(refroute[:id])
     @menu_item = MenuItem.find(params[:id])
     @menu_item.destroy
     redirect_to request.referer
