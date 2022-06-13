5bc148b9dffc32df8ded9009e97c30dfea8bbbc7
determinate1209 <determinate1209@gmail.com>
2022-06-13 08:59:08 +0000

    ブートストラップ、フォントオーサム追加、ブックマーク機能非同期化

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -1,21 +1,18 @@
 class Public::BookmarksController < ApplicationController
   def create
-    menu = Menu.find(params[:menu_id])
-    bookmark = Bookmark.new(menu_id: menu.id)
-    bookmark.customer_id = current_customer.id
-    
+    @menu = Menu.find(params[:menu_id])
+    bookmark = current_customer.bookmarks.new(menu_id: @menu.id)
     bookmark.save
-    redirect_to request.referer
+    
     
   end
   
   def destroy
-    menu = Menu.find(params[:menu_id])
-    bookmark = Bookmark.find_by(menu_id: menu.id)
-    bookmark.customer_id = current_customer.id
+    @menu = Menu.find(params[:menu_id])
+    bookmark = current_customer.bookmarks.find_by(menu_id: @menu.id)
     bookmark.destroy
-    redirect_to request.referer
     
   end
   
 end
+
