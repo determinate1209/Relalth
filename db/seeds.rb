09e0ac4b9a1873bc291b3a30edf219e11970e333
determinate1209 <determinate1209@gmail.com>
2022-06-24 14:19:55 +0000

    修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create!(
-  [
-    {email: 'admin_hoge@test.com', password: 'admin_admin'}
-  ]
-)
 
 customers = Customer.create!(
   [
